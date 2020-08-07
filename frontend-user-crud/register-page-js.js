9e47863b70656583ae0bf507700015da26674aaa
Code Cadet <codecadet@swift.local>
2020-08-07 01:22:14 +0100

    update on register-page-js.js

--- frontend-user-crud/register-page-js.js
+++ frontend-user-crud/register-page-js.js
@@ -1,43 +1,44 @@
-$(document).ready(function() {
 
-    postRegister();
-    getUser();
+function onSuccess(response) {
 
-  });
-
-
-
-
-
-
-
-//add user authenticated
-
-function getUser(){
-
-    var id = $('#nickname')
-    console.log(id);
-
+    alert(response);
 
 }
+
+function errorCallback(request, status, error) {
+
+    console.log(error);
+
+}
+
+$(function () {
+    $("#button1").click(postRegister(event));
+    event.preventDefault();
+});
 
 
 function postRegister() {
-    
+
+    var data = {
+        nickname: $('#nickname').val(),
+        password: $('#password').val(),
+        email: $('#email').val()
+    }
+
     $.ajax({
-    url: 'http://192.168.2.24:8080/BlackPennies/user/',
-    type: 'POST',
-    data: JSON.stringify({
-        nickname: $('nickname'),
-        password: btoa($('password')),
-        email: $('email')
-    }),
-    async: true,
-    contentType: 'application/json',
-    success: onSucess,
-    error: errorCallback
-});
+        url: 'http://192.168.2.24:8080/BlackPennies/user/add',
+        type: 'POST',
+        data: JSON.stringify({
+            nickname: data.nickname,
+            password: data.password,
+            email: data.email
+        }),
+        async: true,
+        contentType: 'application/json',
+        success: onSuccess,
+        error: errorCallback
+    });
 }
 
-  
-  
+
+
